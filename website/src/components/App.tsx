84838b416de7f21f3df1eed557265a421b305b66
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-04-11 18:16:58 +0100

    update

--- website/src/components/App.tsx
+++ website/src/components/App.tsx
@@ -3,13 +3,16 @@ import { LinkList } from "./LinkList"
 
 const App: FC = () => {
   return (
-    <div className="min-h-screen bg-white">
-      <header className="custom-padding flex min-h-screen flex-col items-center p-4">
+    <div className="min-h-screen bg-gradient-red">
+      <header
+        className="flex min-h-screen flex-col items-center p-4 bg-stone-300"
+        style={{ paddingTop: "25vh" }}
+      >
         <h1
           className="mb-10 text-3xl font-bold text-gray-900"
           aria-label="Website Title"
         >
-          Jack Plowman
+          Project Links
         </h1>
         <LinkList />
       </header>

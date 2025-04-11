1d262e1d7a0f700ac26aa89cdec883a7c21e61a6
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-04-11 19:41:30 +0100

    update

--- website/src/components/LinkList.tsx
+++ website/src/components/LinkList.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react"
-import { IconType } from "react-icons";
-import { IoFolderSharp } from "react-icons/io5";
+import { IconType } from "react-icons"
+import { IoFolderSharp } from "react-icons/io5"
 interface LinkItem {
   id: string
   title: string
@@ -13,8 +13,8 @@ const links: LinkItem[] = [
     id: "github-stats",
     title: "github-stats",
     url: "https://github.com/JackPlowman/github-stats",
-    Icon : IoFolderSharp,
-  }
+    Icon: IoFolderSharp,
+  },
 ]
 
 export const LinkList: FC = () => {
@@ -29,7 +29,7 @@ export const LinkList: FC = () => {
             className="flex items-center rounded-lg bg-stone-100 p-4 shadow transition-shadow duration-200 hover:shadow-md"
             aria-label={`Link to ${title}`}
           >
-            <Icon className="mr-4 h-8 w-8"/>
+            <Icon className="mr-4 h-8 w-8" />
             <span className="text-lg text-stone-800">{title}</span>
           </a>
         </li>

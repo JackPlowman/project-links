07315f750e85170215a464b66bc7db6bdc0d094c
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-04-11 17:58:17 +0100

    update

--- website/src/components/App.tsx
+++ website/src/components/App.tsx
@@ -1,32 +1,8 @@
 import { FC } from "react"
+import { LinkList } from "./LinkList"
 
-interface LinkItem {
-  id: string
-  title: string
-  url: string
-  imageUrl: string
-}
 
-const links: LinkItem[] = [
-  {
-    id: "github",
-    title: "GitHub",
-    url: "https://github.com/JackPlowman",
-    imageUrl: "/github-mark.svg",
-  },
-  {
-    id: "linkedin",
-    title: "LinkedIn",
-    url: "https://linkedin.com/in/jackplowman",
-    imageUrl: "/linkedin.svg",
-  },
-  {
-    id: "twitter",
-    title: "Twitter",
-    url: "https://twitter.com/jack_plowman",
-    imageUrl: "/twitter.svg",
-  },
-]
+
 
 const App: FC = () => {
   return (
@@ -38,26 +14,7 @@ const App: FC = () => {
         >
           Jack Plowman
         </h1>
-        <ul className="w-full max-w-md space-y-6" aria-label="List of Links">
-          {links.map(({ id, title, url, imageUrl }) => (
-            <li key={id}>
-              <a
-                href={url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center rounded-lg bg-gray-50 p-4 shadow transition-shadow duration-200 hover:shadow-md"
-                aria-label={`Link to ${title}`}
-              >
-                <img
-                  src={imageUrl}
-                  alt={`${title} logo`}
-                  className="mr-4 h-8 w-8"
-                />
-                <span className="text-lg text-gray-800">{title}</span>
-              </a>
-            </li>
-          ))}
-        </ul>
+        <LinkList />
       </header>
     </div>
   )

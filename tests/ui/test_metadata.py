52ecedb62746a422a1aaae7b7542c1b4601b48d9
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-05-05 13:41:51 +0100

    update

--- tests/ui/test_metadata.py
+++ tests/ui/test_metadata.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import Page
-
+from requests import get
 from .environment_variables import project_url
 
 
@@ -11,3 +11,14 @@ def test_title(page: Page) -> None:
     title = page.title()
     # Assert
     assert title == "Jack's Project Links"
+
+def test_sitemap() -> None:
+    """Test the sitemap of the website."""
+    # Arrange
+    sitemap_url = f"{project_url}/sitemap.xml"
+    # Act
+    response = get(sitemap_url)
+    # Assert
+    assert response.status_code == 200
+    assert response.headers["Content-Type"] == "application/xml"
+    assert "<urlset" in response.text

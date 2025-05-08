82890304ca817dee3b77be86733d40959539968c
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-05-08 19:48:40 +0100

    update

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,6 +1,8 @@
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { generateSitemap } from 'sitemap-ts';
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +10,14 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+        {
+      name: 'generate-sitemap',
+      closeBundle() {
+        generateSitemap({
+          hostname: 'https://jackplowman.github.io/',
+          readable: true,
+        });
+      },
+    }
   ],
 })

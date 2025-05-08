c12eaf24a366a0acb9d1a7a3fbc392ab1ae5fca8
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-05-08 19:45:28 +0100

    update

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,7 +1,6 @@
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import Sitemap from "vite-plugin-sitemap"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,8 +8,5 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    Sitemap({
-      hostname: "https://jackplowman.github.io/project-links",
-    }),
   ],
 })

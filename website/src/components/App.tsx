8b2bf22739d519983aef1e984af55929a57d7edf
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-07-12 18:09:42 +0100

    update

--- website/src/components/App.tsx
+++ website/src/components/App.tsx
@@ -3,11 +3,17 @@ import { LinkList } from "./LinkList"
 
 const App: FC = () => {
   return (
-    <div className="bg-gradient-red min-h-screen">
-      <header
-        className="flex min-h-screen flex-col items-center bg-stone-300 p-4"
-        style={{ paddingTop: "25vh" }}
-      >
+    <div className="relative min-h-screen w-full bg-white">
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: `
+        radial-gradient(125% 125% at 50% 90%, #ffffff 40%, #ec4899 100%)
+      `,
+          backgroundSize: "100% 100%",
+        }}
+      />
+      <div className="relative z-10 flex min-h-screen w-full flex-col place-items-center justify-center">
         <h1
           className="mb-10 text-3xl font-bold text-gray-900"
           aria-label="Website Title"
@@ -15,7 +21,7 @@ const App: FC = () => {
           Project Links
         </h1>
         <LinkList />
-      </header>
+      </div>
     </div>
   )
 }
